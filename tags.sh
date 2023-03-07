9307ac931fac00be7e57fc2a84991ab5b063b967
Daniel Rudolf <github.com@daniel-rudolf.de>
2023-03-07 20:31:51 +0100

    Add GitHub workflow to build & publish container to ghcr.io

---
+++ tags.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+# WeeChat
+# A container running WeeChat, a open-source Internet Relay Chat (IRC) client.
+#
+# Copyright (c) 2023  SGS Serious Gaming & Simulations GmbH
+#
+# This work is licensed under the terms of the MIT license.
+# For a copy, see LICENSE file or <https://opensource.org/licenses/MIT>.
+#
+# SPDX-License-Identifier: MIT
+# License-Filename: LICENSE
+
+set -eu -o pipefail
+export LC_ALL=C
+
+[ -v CI_TOOLS ] && [ "$CI_TOOLS" == "SGSGermany" ] \
+    || { echo "Invalid build environment: Environment variable 'CI_TOOLS' not set or invalid" >&2; exit 1; }
+
+[ -v CI_TOOLS_PATH ] && [ -d "$CI_TOOLS_PATH" ] \
+    || { echo "Invalid build environment: Environment variable 'CI_TOOLS_PATH' not set or invalid" >&2; exit 1; }
+
+source "$CI_TOOLS_PATH/helper/common.sh.inc"
+
+BUILD_DIR="$(CDPATH= cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)"
+source "$BUILD_DIR/container.env"
+
+BUILD_INFO=""
+if [ $# -gt 0 ] && [[ "$1" =~ ^[a-zA-Z0-9_.-]+$ ]]; then
+    BUILD_INFO=".${1,,}"
+fi
+
+# check runtime dependencies
+[ -x "$(which curl)" ] || { echo "Missing runtime dependency: curl" >&2; exit 1; }
+
+# lookup latest stable WeeChat version
+VERSION_URL="https://weechat.org/dev/info/stable/"
+VERSION="$(curl -sSL -f -o - "$VERSION_URL")"
+
+if [ -z "$VERSION" ]; then
+    echo "Unable to read latest WeeChat version: HTTP request '$VERSION_URL' failed" >&2
+    exit 1
+elif ! [[ "$VERSION" =~ ^([0-9]+)\.([0-9]+)(\.([0-9]+))?([+~-]|$) ]]; then
+    echo "Unable to read latest WeeChat version: HTTP request '$VERSION_URL' returned a malformed response: $(head -n1 <<< "$VERSION")" >&2
+    exit 1
+fi
+
+VERSION="${BASH_REMATCH[1]}.${BASH_REMATCH[2]}.${BASH_REMATCH[4]:-0}"
+VERSION_MINOR="${BASH_REMATCH[1]}.${BASH_REMATCH[2]}"
+VERSION_MAJOR="${BASH_REMATCH[1]}"
+
+# build tags
+BUILD_INFO="$(date --utc +'%Y%m%d')$BUILD_INFO"
+
+TAGS=(
+    "v$VERSION" "v$VERSION-$BUILD_INFO"
+    "v$VERSION_MINOR" "v$VERSION_MINOR-$BUILD_INFO"
+    "v$VERSION_MAJOR" "v$VERSION_MAJOR-$BUILD_INFO"
+    "latest"
+)
+
+printf 'VERSION="%s"\n' "$VERSION"
+printf 'TAGS="%s"\n' "${TAGS[*]}"
